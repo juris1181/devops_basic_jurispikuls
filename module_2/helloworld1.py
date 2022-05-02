ada10d13315957cf5729fadf3312410f7c62308a
Juris Pikuls <juris.pikuls@forge.com>
2022-05-02 16:19:42 +0300

    git helloworld1.py pievienosana - commits module_1 un module_2 mapes

---
+++ module_2/helloworld1.py
@@ -0,0 +1,29 @@
+
+
+# lai lietotu nepieciesams: 
+# python -m pip install --upgrade pip
+# pip install paramiko --user
+# pip install librouteros --user
+# pip install paramiko --user
+# pip install librouteros --user
+# pip install paramiko --user
+# pip install librouteros --user
+# pip install paramiko --user
+# pip install librouteros --user
+import os
+from librouteros import connect
+from getpass import getpass
+import json
+import paramiko
+import ipaddress
+from netaddr import IPNetwork
+
+
+
+#izvada "Hello world!"
+
+
+
+print("\n          Hello World!       \n")
+
+quit()
